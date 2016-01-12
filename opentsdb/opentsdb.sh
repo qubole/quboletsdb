e3d43333628196d9286ef3a796e29d3af051fd8f
Rajat Venkatesh <rvenkatesh@qubole.com>
2016-01-12 15:58:41 +0530

    Add Apache v2.0 License

--- opentsdb/opentsdb.sh
+++ opentsdb/opentsdb.sh
@@ -1,3 +1,17 @@
+##
+ # Copyright (c) 2015. Qubole Inc
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ #    limitations under the License.
+##
 #!/bin/bash
 #Heavily inspired by https://github.com/awslabs/emr-bootstrap-actions/blob/master/opentsdb/install-opentsdb.sh
 
@@ -59,7 +73,7 @@ chmod 755 $TSD_HOME/tsdb-status.sh
 #
 echo "Initializing TSD..."
 # check zookeeper connectivity
-for i in 1 2 3 4 5 6 7 8 9; do 
+for i in 1 2 3 4 5 6 7 8 9; do
   echo "zxcv"
   RUOK=`echo ruok | nc -w 5 localhost 2181`
   echo "Wait for zookeeper"
--- qdstsdb/S3.py
+++ qdstsdb/S3.py
@@ -1,3 +1,17 @@
+##
+ # Copyright (c) 2015. Qubole Inc
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ #    limitations under the License.
+##
 __author__ = 'rajatv'
 from boto.s3.connection import S3Connection
 from boto.s3.connection import ProtocolIndependentOrdinaryCallingFormat
@@ -40,4 +54,3 @@ class S3Bucket:
         fp = open(filename)
         key.set_contents_from_file(fp)
         fp.close()
-
